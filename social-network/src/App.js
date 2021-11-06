fe39e7d0e717fa8b732c3dc3e8d28a2747d7a31f
alexle2 <alexle@cmstuning.net>
2021-11-06 14:18:02 +0300

    1-11

---
+++ social-network/src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <header className="header">
+        <img src="" alt="" />
+      </header>
+      <nav className="nav">
+        <div>
+          <a href="">Profile</a>
+        </div>
+        <div>
+          <a href="">Messages</a>
+        </div>
+        <div>
+          <a href="">News</a>
+        </div>
+        <div>
+          <a href="">Music</a>
+        </div>
+        <div>
+          <a href="">Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
